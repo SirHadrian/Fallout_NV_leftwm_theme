18b07aa5566b54e62c3d8b7590b4b47504902448
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-12-11 18:52:49 +0200

    new polybar laucher script to deal with polybar not closing correctly on soft reload

---
+++ polybar_launcher.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# Kill polybar
+kill -9 "$(pgrep -u $UID -x 'polybar')" &>/dev/null
+
+# Wait for polybar to die
+while pgrep -u $UID -x 'polybar' &>/dev/null; do sleep 1; done
+
+# Start polybar
+index=0
+monitors="$(polybar -m | sed s/:.*//)"
+leftwm-state -q -n -t "$SCRIPTPATH"/sizes.liquid | sed -r '/^\s*$/d' | while read -r width x _y; do
+	((indextemp = index + 1))
+	monitor=$(sed "$indextemp!d" <<<"$monitors")
+	barname="mainbar$index"
+	monitor=$monitor offset=$x width=$width polybar -c "$SCRIPTPATH"/polybar.config $barname &>/dev/null &
+	((index = indextemp))
+done
