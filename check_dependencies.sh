c175626398ac5c0955a2ee2e333f29b83612c2b7
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-12-12 18:56:25 +0200

    check_dependencies script to find missing packages

---
+++ check_dependencies.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR="$(cd "$(dirname "$0")" || exit && pwd -P)"
+
+# Get colors
+source "$SCRIPTDIR"/colors.sh
+
+# Check for hard dependencies
+declare -a dependencies=(
+	"polybar"
+	"rofi"
+	"leftwm"
+	"checkupdates"
+	"greenclip"
+	"dunst"
+	"picom"
+	"unclutter"
+	"optimus-manager-qt"
+	"amixer"
+	"light"
+	"alacritty"
+	"xlock"
+	"bluetoothctl"
+	"paru"
+	"nmcli"
+	"feh"
+	"wal"
+	"cmus"
+)
+
+printf "\n${BWhite}%s" "================================"
+printf "\n%s" "|      Check Dependencies      |"
+printf "\n%s${Color_Off}" "================================"
+printf "\n\n"
+
+for item in "${dependencies[@]}"; do
+	printf "${BWhite}%-20s      " "${item}"
+	if [[ -x "$(command -v "$item")" ]]; then
+		printf "${BGreen}%s${Color_Off}\n" "Ok.."
+	else
+		printf "${BRed}%s${Color_Off}\n" "Missing.."
+	fi
+done
+
+printf "\n%s\n" "Tip: use pacman -F <command> to find the package the command is from"
