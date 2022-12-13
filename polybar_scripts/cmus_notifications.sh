e9392ca54740e6b69b1c1174cddc78f8b7704e35
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-12-13 04:14:27 +0200

    working cmus module notifications using dunst

--- polybar_scripts/cmus_notifications.sh
+++ polybar_scripts/cmus_notifications.sh
@@ -3,13 +3,15 @@
 # Notifications
 ARTIST="$(cmus-remote -C status | grep "tag artist" | cut -c 12-)"
 SONG="$(cmus-remote -C status | grep title | cut -c 11-)"
-CURRENTLY_PLAYING="$(cmus-remote -C status | grep "file" | awk '{print $2}')"
+CURRENTLY_PLAYING="$(cmus-remote -C status | grep "file" | cut -c 6-)"
 FILENAME="$(basename "${CURRENTLY_PLAYING}")"
 COVERSDIR="$HOME/Music/covers"
 
 [[ -d ${COVERSDIR} ]] || mkdir -p "${COVERSDIR}"
 
 if ! [[ -f "${COVERSDIR}/${FILENAME}" ]]; then
-	ffmpeg -i "${CURRENTLY_PLAYING}" -an -c:v copy "${COVERSDIR}/${FILENAME}.jpg" &>/dev/null &&
-		dunstify --raw_icon="${COVERSDIR}/${FILENAME}" "${ARTIST}" "${SONG}"
+	ffmpeg -i "${CURRENTLY_PLAYING}" -an -c:v copy "${COVERSDIR}/${FILENAME}.jpg" &>/dev/null
 fi
+
+sleep 1
+dunstify --raw_icon="${COVERSDIR}/${FILENAME}.jpg" "${ARTIST}" "${SONG}"
