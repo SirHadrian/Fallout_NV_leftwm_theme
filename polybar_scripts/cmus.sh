9f365e8cee29b3ee600f344c6a2c78e923b09715
Toma Adrian Ionut <sirhadrian21@gmail.com>
2023-03-14 16:53:13 +0200

    cmus polybar script removed time

--- polybar_scripts/cmus.sh
+++ polybar_scripts/cmus.sh
@@ -4,22 +4,23 @@ prepend_zero() {
 	seq -f "%02g" "$1" "$1"
 }
 
-artist=$(echo -n "$(cmus-remote -C status | grep "tag artist" | cut -c 12-)")
+artist=$(printf "%s" "$(cmus-remote -C status | grep "tag artist" | cut -c 12-)")
 
 if [[ -z "$artist" ]]; then
-	echo -n "$(cmus-remote -C status | grep file | cut -c 6-)"
+	printf "%s" "$(cmus-remote -C status | grep file | cut -c 6-)"
 	exit
 fi
 
 if [[ $artist = *[!\ ]* ]]; then
-	song=$(echo -n "$(cmus-remote -C status | grep title | cut -c 11-)")
-	position=$(cmus-remote -C status | grep position | cut -c 10-)
-	minutes1=$(prepend_zero $((position / 60)))
-	seconds1=$(prepend_zero $((position % 60)))
-	duration=$(cmus-remote -C status | grep duration | cut -c 10-)
-	minutes2=$(prepend_zero $((duration / 60)))
-	seconds2=$(prepend_zero $((duration % 60)))
-	echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+	song=$(printf "%s" "$(cmus-remote -C status | grep title | cut -c 11-)")
+	# position=$(cmus-remote -C status | grep position | cut -c 10-)
+	# minutes1=$(prepend_zero $((position / 60)))
+	# seconds1=$(prepend_zero $((position % 60)))
+	# duration=$(cmus-remote -C status | grep duration | cut -c 10-)
+	# minutes2=$(prepend_zero $((duration / 60)))
+	# seconds2=$(prepend_zero $((duration % 60)))
+	# echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
+	printf "%s" "$artist - $song"
 else
 	echo
 fi
