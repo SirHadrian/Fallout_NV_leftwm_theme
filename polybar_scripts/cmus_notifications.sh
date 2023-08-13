808fd84c9ee14a68a7b4aa7d0803cf0c9020ee0a
Toma Adrian <sirhadrian21@gmail.com>
2023-08-13 18:06:50 +0300

    fixed no notification displayed on prev song

--- polybar_scripts/cmus_notifications.sh
+++ polybar_scripts/cmus_notifications.sh
@@ -9,9 +9,10 @@ COVERSDIR="$HOME/Music/covers"
 
 [[ -d ${COVERSDIR} ]] || mkdir -p "${COVERSDIR}"
 
-if ! [[ -e "${COVERSDIR}/${FILENAME}.jpeg" ]]; then
+if ! [[ -f "${COVERSDIR}/${FILENAME%.mp3}.jpeg" ]]; then
 	ffmpeg -i "${CURRENTLY_PLAYING}" -an -filter scale=128:-1 "${COVERSDIR}/${FILENAME%.mp3}.jpeg" &>/dev/null || exit
-	sleep 2
+	# Wait for system IO
+	sleep 1
 fi
 
 dunstify --raw_icon="${COVERSDIR}/${FILENAME%.mp3}.jpeg" "${ARTIST}" "${SONG}"
